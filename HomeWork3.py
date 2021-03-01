e3b7cb2dc98be22bb47bf17037ca5438b82e0515
Максим <79326174+maksymsharko@users.noreply.github.com>
2021-03-01 09:21:47 +0200

    Update HomeWork3.py

--- HomeWork3.py
+++ HomeWork3.py
@@ -33,7 +33,7 @@ print("Anna has {1} apples and {0} peaches".format(numpeaches, numapples))
 print("Anna has {apples} apples and {peaches} peaches".format(apples = numapples, peaches = numpeaches))
 
 #8*. With indicators of field size(5 chars for the first and 3 for the second)
-print("Anna has {0:5} apples and {1:3} peaches".format(3, 5))
+print("Anna has {0:.5} apples and {1:.3} peaches".format("three", "five"))
 
 #9. With f-strings and variables
 apples = 11
